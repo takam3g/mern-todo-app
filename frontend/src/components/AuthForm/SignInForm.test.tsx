239a29b00f4eaa1f5cc4f95b4b08fb990ea2ced6
Megumi Takashima <mtakashima00@mylangara.ca>
2024-02-22 07:27:33 -0800

    Add test error messages

--- frontend/src/components/AuthForm/SignInForm.test.tsx
+++ frontend/src/components/AuthForm/SignInForm.test.tsx
@@ -22,6 +22,25 @@ describe('SignInForm', () => {
     expect(passwordInput).toBeInTheDocument();
   });
 
+  test('Should be able to type username and password', () => {
+    render(
+      <MemoryRouter>
+        <SignInForm />
+      </MemoryRouter>
+    );
+
+    const usernameInput: HTMLInputElement = screen.getByLabelText(/username/i);
+    const passwordInput: HTMLInputElement = screen.getByLabelText(/password/i);
+
+    const username = 'testuser';
+    const password = 'testpassword';
+    fireEvent.change(usernameInput, { target: { value: username } });
+    fireEvent.change(passwordInput, { target: { value: password } });
+
+    expect(usernameInput.value).toBe(username);
+    expect(passwordInput.value).toBe(password);
+  });
+
   test('validates feilds and submit the form', () => {
     // Mock onSubmit function
     const mockSubmit = jest.fn();
@@ -78,8 +97,8 @@ describe('SignInForm', () => {
     const passwordInput = screen.getByLabelText(/password/i);
     const signInButton = screen.getByRole('button', { name: /sign in/i });
 
-    // Fill out the feilds
-    const username = 'testuser';
+    // Fill out the feilds (empty input)
+    const username = '';
     const password = '';
     fireEvent.change(usernameInput, { target: { value: username } });
     fireEvent.change(passwordInput, { target: { value: password } });
@@ -98,6 +117,10 @@ describe('SignInForm', () => {
 
     // Validation should not pass
     expect(validateUserInput(username, password, jest.fn())).not.toBe(true);
+
+    // It should display error messages
+    expect(screen.getByText(/username is required/i)).toBeInTheDocument();
+    expect(screen.getByText(/password is required/i)).toBeInTheDocument();
 
     // It should not submit the form
     expect(mockSubmit).not.toHaveBeenCalled();
